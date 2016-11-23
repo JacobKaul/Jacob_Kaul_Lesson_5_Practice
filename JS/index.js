ff3baff099b04c624098ebeda9aaf3c6ecd772b0
Jacob H. Kaul <JacobKaul@jacobs-mbp.cvtc.local>
2016-11-23 12:29:48 -0600

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,46 @@
+// #1
+  function bitwiseAnd(x, y)
+  {
+    return x & y;
+  };
+  
+    function bitwiseOr(x, y)
+  {
+    return x | y;
+  };
+  
+  function bitwiseXor(x, y)
+  {
+    return x ^ y;
+  };
+  
+    function bitwiseLShift(x, y)
+  {
+    return x << y;
+  };
+  
+      function bitwiseRShift(x, y)
+  {
+    return x >> y;
+  };
+// #2
+  function removeSubStr(text, subStr) 
+  {
+    return text.split(subStr).join("");
+  };
+  
+// #4
+  function reverse(text2)
+  {
+    return text2.split("").reverse().join("");
+  }
+// #5
+  if (number1 < number2){
+  
+  }
+  
+  
+  
+  
+  
+// #6
